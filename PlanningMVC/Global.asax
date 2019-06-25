1d9c0170f08c37735454a088f0cb0aedbcf9894a
Pavel <pavelquevedo@outlook.com>
2019-06-24 22:01:25 -0600

    Add project files.

---
+++ PlanningMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlanningMVC.MvcApplication" Language="C#" %>
---
+++ PlanningMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlanningMVC.Startup))]
+namespace PlanningMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
